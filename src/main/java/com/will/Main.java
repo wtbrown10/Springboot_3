23605f82bb6d7afa676c441140260189b8e54872
Will brown <wtbrown10@gmail.com>
2022-12-03 20:34:09 -0500

    first commit

---
+++ src/main/java/com/will/Main.java
@@ -0,0 +1,30 @@
+package com.will;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+
+    }
+    @GetMapping("/")
+    public GreetResponse greet() {
+         GreetResponse response = new GreetResponse("Hello!",
+                List.of("Java", "Golang", "Python", "JavaScript"),
+                new Person("Alex", 28, 30500));
+         return response;
+    }
+
+    record Person(String name, int age, double savings){
+
+    }
+    record GreetResponse(String greet, List<String> favProgrammingLanguages, Person person){
+    }
+}
